e190ac5b36516919632e0025d45cfcfd5797bdd9
karthik_uandme <karthik.b@uandme.org>
2018-07-27 10:09:32 +0530

    async to fetch

--- erc20/e-scanDecimal.js
+++ erc20/e-scanDecimal.js
@@ -1,81 +1,36 @@
 var scraperjs = require('scraperjs');
-var fs = require('fs');
-var async = require("async");
-var obj = "";
-var counter = 0;
-
-var info = [];
-var x = [];
-
-
-
-async function readFile(path, opts = 'utf8') {
-    new Promise((res, rej) => {
-        fs.readFile(path, opts, (err, data) => {
-            if (err) rej(err)
-            else res(data)
-        })
-    });
-}
-console.log(__dirname+"/erc20.json");
-
-readFile(__dirname+"/erc20.json").then(function(e){
-    console.log(e);
-}).catch(function(e){
-    console.log(e);
-});
-
-// fs.readFile(__dirname + "/erc20.json", 'utf8', async function (err, data) {
-//   aw obj = JSON.parse(data);
-//     //  async.eachSeries(JSON.parse(data), async function (value, callback) {
-//     //     await createPromise(value).then( function (h) {             
-//     //         scraperjs.StaticScraper.create(h.url)
-//     //             .scrape(function ($) {
-//     //                 return $("#ContentPlaceHolder1_trDecimals td:last-child").map(function () {
-//     //                     var decimal = $(this).text();
-//     //                     var od = {};
-//     //                     od['decimal'] = decimal.replace(/^\s*|\s*$/g, '');
-//     //                     od['address'] = h.address;
-//     //                     od['erc20name'] = h.erc20name;
-//     //                     return od;
-//     //                 }).get();
-//     //             })
-//     //             .then(function (erc20) {
-//     //                 if (erc20.length > 0) {
-//     //                     info.push(erc20);
-//     //                     var json = JSON.stringify(info, null, 2);
-//     //                     var jsonFile = '/erc20DFJ.json';
-//     //                     fs.writeFile(__dirname + jsonFile, json, 'utf8');
-//     //                 }
-
-//     //             });
-//     //     }).catch(function (e) {
-//     //         console.log(e);
-//     //     });
-//     // })
-
-// });
-
-
-function createPromise(obj) {
-
-    return new Promise(function (resolve, reject) {
-        if (obj) {
-            var url = 'https://etherscan.io/' + obj.address;
-            var d = {};
-            d['url'] = url;
-            d['erc20name'] = obj.erc20name;
-            d['address'] = obj.address;
-            resolve(d);
-        } else {
-            reject(0);
+var fs = require('fs-extra');
+(async function main() {
+    try {
+        const addressRaw = await fs.readFile(__dirname + "/erc20.json", 'utf-8');
+        const addressJson = JSON.parse(addressRaw);
+        const result = await Promise.all(addressJson.map(async (decimal) => {
+            try {
+                var url = "https://etherscan.io/" + decimal.address;
+                const res = await scraperjs.StaticScraper.create(url)
+                    .scrape(function ($) {
+                        return $("#ContentPlaceHolder1_trDecimals td:last-child").map(function () {
+                            var d = $(this).text();
+                            var od = {};
+                            od['decimal'] = d.replace(/^\s*|\s*$/g, '');
+                            od['address'] = decimal.address;
+                            od['erc20name'] = decimal.erc20name;
+                            return od;
+                        }).get();
+                    }).catch(function(e){
+                        console.log(e);
+                    });
+                return res;
+            } catch (e) {
+                console.log(e.message)
+            }
+        }));
+        if (result.length > 0) {
+            var json = JSON.stringify(result, null, 2);
+            fs.writeFile(__dirname + '/erc20DFJ.json', json, 'utf8');
         }
-    });
 
-}
-
-function asyncronous(obj) {
-    console.log(obj);
-    //     var url = 'https://etherscan.io/';           
-
-}
+    } catch (e) {
+        console.log(e.message);
+    }
+})();
